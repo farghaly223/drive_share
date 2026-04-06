ba8dde69c91b3173d40f3432f1ff981043c0b9db
safwa245 <168284520+safwa245@users.noreply.github.com>
2026-04-06 22:16:29 +0200

    Add files via upload

---
+++ cars_rental/DTOs/CarCreateUpdateDTOs.cs
@@ -0,0 +1,12 @@
+public class CarCreateUpdateDto
+{
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public string? CarType { get; set; }
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+    public int? Year { get; set; }
+    public string? Transmission { get; set; }
+    public string? Location { get; set; }
+    public decimal? RentalPrice { get; set; }
+}
